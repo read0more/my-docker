54f4ee5516ef7c95ae1005ab0519760d3020ff43
ykpark <read0more@gmail.com>
2022-03-15 16:52:26 +0900

    X11로 하려던 부분 여러 이슈로 폐기

--- before_docker_compose.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-IP=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}') && export DISPLAY=$IP:0 
-open -a XQuartz && exec Xhost $IP &
-exec docker-compose "$@"
-
--- shell.sh
+++ shell.sh
@@ -1,1 +1,1 @@
-docker-compose exec --user root php-fpm  /bin/sh -c "cd /var/www; exec bash -l"
+docker-compose exec --user root php-fpm-x86_64  /bin/sh -c "cd /var/www; exec bash -l"
